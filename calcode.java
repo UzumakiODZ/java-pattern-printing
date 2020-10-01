a0d57b18b6ed59a8869fdbb20e22e3efed4ba4db
UzumakiODZ <72137639+UzumakiODZ@users.noreply.github.com>
2020-10-01 13:21:38 +0530

    Update calcode.java
    I have removed some spelling mistakes and an error where your program was not giving exact percentage.

--- calcode.java
+++ calcode.java
@@ -2,8 +2,8 @@ package com.company;
 import java.util.Scanner;
 
 public class exersise_1 {
-    public static void main(String[] args){
-        System.out.println("cbsc exam result program:-");
+    public static void main(String[] args){
+System.out.println("cbse exam result program:-");
         Scanner s=new Scanner(System.in);
         System.out.println("marks of Hindi :- ");
         int Hindi=s.nextInt();
@@ -16,11 +16,12 @@ public class exersise_1 {
         System.out.println("marks of Science :- ");
         int Science=s.nextInt();
         System.out.println("Total marks are out of Hundred :- ");
-        int Total_marks=(Hindi+English+Maths+Computer+Science);
+        float Total_marks=(Hindi+English+Maths+Computer+Science);
         System.out.println(Total_marks);
-        System.out.println("Total percentaage :-");
+        System.out.println("Total percentage :-");
         float Total_prcentage=(Total_marks*100/500);
+        
         System.out.println(Total_prcentage);
-    }
+    }
 
 }
